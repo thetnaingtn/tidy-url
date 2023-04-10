ace353b91e3271ef3a47d62a4fbc4ea499c17db0
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-04-10 21:56:00 +0700

    update initial ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import IconSend from "./icons/IconSend";
+import IconCopy from "./icons/IconCopy";
 
 function App() {
-  return <p className="text-red-600">Generate a new tidy url </p>
+  return (
+    <div className="w-screen h-screen bg-[#ccebe3] flex flex-col justify-center items-center">
+      <div className="flex">
+        <input
+          type="text"
+          className="outline-none bg-slate-50 border-solid border-[#a5c3c2] border rounded-md w-96 h-9 text-ellipsis"
+        />
+        <button type="button" className="ml-[-30px]">
+          <IconSend />
+        </button>
+      </div>
+      <div className="flex">
+        <p>test</p>
+        <button type="button">
+          <IconCopy />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
