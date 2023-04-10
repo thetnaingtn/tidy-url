36b85efeca81ab222b3ab92a68810ec15223be4d
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-04-10 19:17:08 +0700

    update Handlers type and add new static route

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,19 +1,31 @@
 package handlers
 
 import (
+	"io/fs"
+	"net/http"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/julienschmidt/httprouter"
 	"github.com/thetnaingtn/tidy-url/core"
 )
 
-func InitializeRouter(db *sqlx.DB) *httprouter.Router {
+type Handlers struct {
+	core core.Core
+	uiFS fs.FS
+}
+
+func InitializeRouter(db *sqlx.DB, uiFS fs.FS) *httprouter.Router {
 	router := httprouter.New()
 
 	base := Handlers{
 		core: core.NewCore(db),
+		uiFS: uiFS,
 	}
 
-	router.GET("/:id", base.Expand)
+	router.ServeFiles("/assets/*filepath", http.Dir("ui/dist/assets"))
+
+	router.GET("/", base.StaticHandler)
+	router.GET("/expand/:id", base.Expand)
 	router.POST("/tidy", base.Tidy)
 
 	return router
