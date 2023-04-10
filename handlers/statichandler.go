57343f8ad2b72ba38def61dbb6f612c484594dc5
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-04-10 19:14:03 +0700

    add static handler

---
+++ handlers/statichandler.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (h Handlers) StaticHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	path := filepath.Clean(r.URL.Path)
+	if path == "/" { // Add other paths that you route on the UI side here
+		path = "index.html"
+	}
+	path = strings.TrimPrefix(path, "/")
+
+	file, err := h.uiFS.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Println("file", path, "not found:", err)
+			http.NotFound(w, r)
+			return
+		}
+		log.Println("file", path, "cannot be read:", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	contentType := mime.TypeByExtension(filepath.Ext(path))
+	w.Header().Set("Content-Type", contentType)
+	if strings.HasPrefix(path, "static/") {
+		w.Header().Set("Cache-Control", "public, max-age=31536000")
+	}
+	stat, err := file.Stat()
+	if err == nil && stat.Size() > 0 {
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", stat.Size()))
+	}
+
+	n, _ := io.Copy(w, file)
+	log.Println("file", path, "copied", n, "bytes")
+}
