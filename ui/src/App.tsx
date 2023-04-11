b2546f06016cbb6c292fe7d9bc27d903c8754dda
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-04-11 17:43:13 +0700

    adjust color

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
             setLongUrl(e.target.value);
           }}
           placeholder="Paste Your Long URL Here..."
-          className="outline-none bg-slate-50 border-solid border-[#a5c3c2] border rounded-md w-96 h-9 text-ellipsis py-2 pl-2 pr-7"
+          className="outline-none bg-slate-50 border-solid border-[#a5c3c2] border rounded-md w-96 h-9 text-ellipsis py-2 pl-2 pr-7 text-[#a5c3c2]"
         />
         <button
           type="button"
@@ -41,7 +41,7 @@ function App() {
       </div>
       {data && (
         <div className="flex p-1">
-          <span className="text-[#a5c3c2]">{data.short_url}</span>
+          <span className="text-[#74a09e]">{data.short_url}</span>
           <button className="ml-2" type="button">
             <IconCopy />
           </button>
