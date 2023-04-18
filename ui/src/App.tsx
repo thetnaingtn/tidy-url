c11f8e17dabe41c5a84a88639ed4e2c2405ac401
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-04-18 23:26:47 +0700

    update ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -29,11 +29,11 @@ function App() {
             setLongUrl(e.target.value);
           }}
           placeholder="Paste your long URL here..."
-          className="outline-none bg-slate-50 border-solid border-[#a5c3c2] border rounded-md w-96 h-9 text-ellipsis py-2 pl-2 pr-7 text-[#a5c3c2]"
+          className="outline-none bg-slate-50 border-solid border-[#a5c3c2] border rounded-md w-96 h-11 text-ellipsis py-2 pl-2 pr-7 text-[#a5c3c2]"
         />
         <button
           type="button"
-          className="absolute right-2 top-[0.406rem] disabled:cursor-not-allowed"
+          className="absolute right-2 top-[0.6875rem] disabled:cursor-not-allowed"
           disabled={!longUrl}
           onClick={handleTidyUp}
         >
