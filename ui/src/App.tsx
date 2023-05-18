badbaadf263ec757b4a48c0cba795cd65852028a
Thet Naing Tun <thetnaingtun.ucsy@gmail.com>
2023-05-19 01:32:06 +0700

    feat: add github corner component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,6 +4,7 @@ import useSWRMutation from "swr/mutation";
 import IconSend from "./icons/IconSend";
 import IconCopy from "./icons/IconCopy";
 import fetcher from "./fetcher";
+import GithubCorner from "./GithubCorner";
 
 function App() {
   const { data, trigger } = useSWRMutation(
@@ -20,47 +21,50 @@ function App() {
   };
 
   return (
-    <div className="w-screen h-screen bg-[#ccebe3] flex flex-col justify-center items-center gap-3">
-      <div className="flex relative">
-        <input
-          type="text"
-          value={longUrl}
-          onChange={(e) => {
-            setLongUrl(e.target.value);
-          }}
-          placeholder="Paste your long URL here..."
-          className="outline-none bg-slate-50 border-solid border-[#a5c3c2] border rounded-md w-96 h-11 text-ellipsis py-2 pl-2 pr-7 text-[#a5c3c2]"
-        />
-        <button
-          type="button"
-          className="absolute right-2 top-[0.6875rem] disabled:cursor-not-allowed"
-          disabled={!longUrl}
-          onClick={handleTidyUp}
-        >
-          <IconSend />
-        </button>
-      </div>
-      {data && (
-        <div className="flex">
+    <>
+      <GithubCorner />
+      <div className="w-screen h-screen bg-[#ccebe3] flex flex-col justify-center items-center gap-3">
+        <div className="flex relative">
           <input
-            readOnly
-            ref={result}
-            className="text-[#74a09e] bg-transparent outline-none w-96"
-            value={data.short_url}
-          />
-
-          <button
-            onClick={() => {
-              result.current?.select();
-              document.execCommand("copy");
+            type="text"
+            value={longUrl}
+            onChange={(e) => {
+              setLongUrl(e.target.value);
             }}
+            placeholder="Paste your long URL here..."
+            className="outline-none bg-slate-50 border-solid border-[#a5c3c2] border rounded-md w-96 h-11 text-ellipsis py-2 pl-2 pr-7 text-[#a5c3c2]"
+          />
+          <button
             type="button"
+            className="absolute right-2 top-[0.6875rem] disabled:cursor-not-allowed"
+            disabled={!longUrl}
+            onClick={handleTidyUp}
           >
-            <IconCopy />
+            <IconSend />
           </button>
         </div>
-      )}
-    </div>
+        {data && (
+          <div className="flex">
+            <input
+              readOnly
+              ref={result}
+              className="text-[#74a09e] bg-transparent outline-none w-96"
+              value={data.short_url}
+            />
+
+            <button
+              onClick={() => {
+                result.current?.select();
+                document.execCommand("copy");
+              }}
+              type="button"
+            >
+              <IconCopy />
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
